14b429eda50283c4841574c3dc2e9161fff37bcc
lam-brian <brianlam408@gmail.com>
2024-05-12 04:26:41 -0700

    add mobile nav

--- script2.js
+++ script2.js
@@ -2,10 +2,11 @@
 
 const heroSection = document.querySelector(".hero");
 const header = document.querySelector(".header");
-// const hamburgerBtn = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const hamburgerBtn = document.querySelector(".hamburger");
 const navLinks = document.querySelectorAll(".nav__links");
 
-// Stick header
+// Sticky header
 const headerHeight = header.getBoundingClientRect().height;
 
 const stickyHeader = function (entries) {
@@ -20,3 +21,16 @@ const heroObserver = new IntersectionObserver(stickyHeader, {
   rootMargin: `-${headerHeight}px`,
 });
 heroObserver.observe(heroSection);
+
+// Mobile Navigation
+const openNav = function () {
+  hamburgerBtn.classList.toggle("is-active");
+  nav.classList.toggle("nav--open");
+};
+
+hamburgerBtn.addEventListener("click", openNav);
+navLinks.forEach((link) => link.addEventListener("click", openNav));
+
+// Footer year
+const year = document.querySelector(".footer__year");
+year.textContent = new Date().getFullYear();
